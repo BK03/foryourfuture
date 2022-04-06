b8993a35523a0374295839224f27d7c54d3721c7
Bala Kumar <80034742+BK03@users.noreply.github.com>
2022-04-06 16:48:57 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
+import React from "react";
+
+
+import Body from "./components/Body/Body";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     
+     <Body />
     </div>
   );
 }
